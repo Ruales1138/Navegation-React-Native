b81c1c47ed200bb5add3c684c1033bdb72b70104
unknown <hruales1138@gmail.com>
2023-06-17 16:29:06 -0500

    commit

--- App.js
+++ App.js
@@ -20,16 +20,35 @@ function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button title='Go to Details' onPress={() => navigation.navigate('Details')}/>
+      <Button 
+        title='Go to Details' 
+        onPress={() => {
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here'
+          })
+        }}
+      />
     </View>
   )
 };
 
-function DetailsScreen({ navigation }) {
+function DetailsScreen({ route, navigation }) {
+  const { itemId, otherParam } = route.params;
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Button title='Go to Details... again' onPress={() => navigation.push('Details')}/>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button 
+        title='Go to Details... again' 
+        onPress={() => {
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100)
+          })
+        }}
+      />
       <Button title='Go to Home' onPress={() => navigation.navigate('Home')}/>
       <Button title='Go back' onPress={() => navigation.goBack()}/>
       <Button title='Go back to first screen in stack' onPress={() => navigation.popToTop()}/>
